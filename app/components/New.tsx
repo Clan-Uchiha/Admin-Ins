d22f1c0bd89fd9a1d34f7439b855ed3f56da9802
Sai Adithya <91713156+SaiAdithya3@users.noreply.github.com>
2023-10-13 11:15:30 +0530

    Create New.tsx

---
+++ app/components/New.tsx
@@ -0,0 +1,10 @@
+import react from "React"
+
+const New: React.FC<FormInputProps> = ({
+  return (
+    <>
+                                             <h1>Yo</h1>
+    </>
+  )
+  })
+export default New;
